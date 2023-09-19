2bd37cbfe00592e3a5966564fc795610b47edf2e
PradipJathar <jatharpradipofficial@gmail.com>
2023-09-19 19:22:12 +0530

    Classes

---
+++ Basic/Class/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void Introduce()
+        {
+            Console.WriteLine($"Hi, My name is {FirstName} {LastName}.");
+        }
+    }
+}

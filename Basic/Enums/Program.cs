952123ae08e3b76bab9944e1bd94abd5a70c7e86
PradipJathar <jatharpradipofficial@gmail.com>
2023-09-20 15:38:44 +0530

    Enums

---
+++ Basic/Enums/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);                 // enum to int
+            Console.WriteLine(method.ToString());           // enum to string
+
+            var methodId = 2;
+            Console.WriteLine((ShippingMethod)methodId);    // int to enum
+
+            var methodName = "RegularAirMail";
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);    // string to enum
+            Console.WriteLine((int)shippingMethod);         // enum to int
+        }
+    }
+}

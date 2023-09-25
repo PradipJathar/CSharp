0edb2f7bb2dd5e61da3f4982cc05358a1c81b31d
PradipJathar <jatharpradipofficial@gmail.com>
2023-09-25 15:55:11 +0530

    Classes

---
+++ Intermediate/Classes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Object
+            Person person = new Person();
+            person.Name = "Pradip";
+            person.Introduce("Abhi");
+
+
+            // Parse to person
+            Person p = Person.Parse("Rohan");
+            p.Introduce("Abhi");
+        }
+    }
+}

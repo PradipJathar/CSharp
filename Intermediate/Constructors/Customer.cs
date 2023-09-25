6a11838613f227133fa2e3bc8180a71d9f532a13
PradipJathar <jatharpradipofficial@gmail.com>
2023-09-25 16:13:13 +0530

    Constructors

---
+++ Intermediate/Constructors/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Constructors
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Order> Orders { get; set; }
+
+
+        // Default constructor
+        public Customer()
+        {
+            this.Orders = new List<Order>();
+        }
+
+
+        // Constructor with one parameter
+        public Customer(int id) : this()
+        {
+            this.Id = id;
+        }
+
+
+         // Constructor with two parameter
+        public Customer(int id, string Name): this(id)
+        {
+            this.Name = Name;
+        }
+    }
+}
---
+++ Intermediate/Constructors/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var order = new Order();
+
+            // Creating object of class using Default constructor
+            Customer customer = new Customer();
+            customer.Orders.Add(order);
+            Console.WriteLine($"Id: {customer.Id}");
+            Console.WriteLine($"Name: {customer.Name}");
+
+            // Creating object of class using Constructor with one parameter
+            Customer customer1 = new Customer(1);
+            customer1.Orders.Add(order);
+            Console.WriteLine($"Id: {customer1.Id}");
+            Console.WriteLine($"Name: {customer1.Name}");
+
+            // Creating object of class using Constructor with two parameter
+            Customer customer2 = new Customer(2, "Pradip");
+            customer2.Orders.Add(order);
+            Console.WriteLine($"Id: {customer2.Id}");
+            Console.WriteLine($"Name: {customer2.Name}");
+        }
+    }
+}

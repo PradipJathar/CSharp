17be87874ef55c3bdda84d7526896b4533c7cb76
PradipJathar <jatharpradipofficial@gmail.com>
2023-09-25 19:06:23 +0530

    Access Modifiers And Properties.

---
+++ Intermediate/AccessModifiersAndProperties/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AccessModifiersAndProperties
+{
+    public class Person
+    {
+        public DateTime Birtdate { get; private set; }
+
+        public Person(DateTime birthdate)
+        {
+            this.Birtdate = birthdate;
+        }
+
+        public int Age 
+        { 
+            get 
+            {
+                TimeSpan timeSpan = DateTime.Today - Birtdate;
+
+                return timeSpan.Days / 365;
+            }
+        }
+
+
+    }
+}
---
+++ Intermediate/AccessModifiersAndProperties/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiersAndProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person(new DateTime(1999, 03, 01));
+
+            // person.Birtdate = new DateTime(1999, 03, 01);        // Not allowed to set birthdate because of private setter
+
+            Console.WriteLine(person.Age);
+
+            // person.Age = 23;            //  Age became readonly because setter is not declared for age.
+        }
+    }
+}

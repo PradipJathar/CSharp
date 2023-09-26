d83f71052e80dd332b3dd195a78a6ff9906688e8
PradipJathar <jatharpradipofficial@gmail.com>
2023-09-26 13:37:14 +0530

    Classes Exercise - Stopwatch

---
+++ Intermediate/ClassesExerciseStopwatch/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ClassesExerciseStopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+
+            while (true)
+            {
+                Console.Write("Enter a command: 'start', 'stop', 'quit': ");
+                var input = Console.ReadLine();
+
+                switch (input.ToLower())
+                {
+                    case "start":
+                        try
+                        {
+                            stopwatch.Start();
+                            Console.WriteLine("Stopwatch started.");
+                        }
+                        catch (InvalidOperationException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+
+                    case "stop":
+                        try
+                        {
+                            stopwatch.Stop();
+                            var duration = stopwatch.GetDuration();
+                            if (duration.HasValue)
+                            {
+                                Console.WriteLine($"Duration: {duration.Value.TotalSeconds} seconds");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Stopwatch hasn't been started yet.");
+                            }
+                        }
+                        catch (InvalidOperationException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+
+                    case "quit":
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid command. Try again.");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Intermediate/ClassesExerciseStopwatch/Stopwatch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ClassesExerciseStopwatch
+{
+    public class Stopwatch
+    {
+        private DateTime? startTime;
+        private DateTime? stopTime;
+        private bool isRunning;
+
+        public void Start()
+        {
+            if (isRunning)
+            {
+                throw new InvalidOperationException("Stopwatch is already running.");
+            }
+
+            startTime = DateTime.Now;
+            isRunning = true;
+        }
+
+        public void Stop()
+        {
+            if (!isRunning)
+            {
+                throw new InvalidOperationException("Stopwatch is not running.");
+            }
+
+            stopTime = DateTime.Now;
+            isRunning = false;
+        }
+
+        public TimeSpan? GetDuration()
+        {
+            if (startTime.HasValue && stopTime.HasValue)
+            {
+                return stopTime.Value - startTime.Value;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}

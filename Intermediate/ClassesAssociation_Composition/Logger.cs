06efecf9a32ea43fffbd43657485c0c43a72241b
PradipJathar <jatharpradipofficial@gmail.com>
2023-09-26 19:31:34 +0530

    Association Between Classes - Composition.

---
+++ Intermediate/ClassesAssociation_Composition/Logger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassesAssociation_Composition
+{
+    public class Logger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Intermediate/ClassesAssociation_Composition/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAssociation_Composition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DbMigrator dbMigrator = new DbMigrator(new Logger());    // Directly create new Logger Object in the constructor 
+
+            Logger logger = new Logger();                           // Logger Object created outside and assign in the constructor
+            Installer installer = new Installer(logger);
+
+            dbMigrator.Migrate();
+
+            installer.Install();
+        }
+    }
+}

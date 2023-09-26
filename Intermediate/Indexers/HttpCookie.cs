22835c09674aa24c4043eb14788c7454c2d6c858
PradipJathar <jatharpradipofficial@gmail.com>
2023-09-26 13:29:08 +0530

    Indexers

---
+++ Intermediate/Indexers/HttpCookie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Indexers
+{
+    public class HttpCookie
+    {
+        private readonly Dictionary<string, string> _dictionary;
+        public DateTime Expiry { get; set; }
+
+        public HttpCookie()
+        {
+            _dictionary = new Dictionary<string, string>();
+        }
+
+        // Indexer declaration
+        public string this[string key] 
+        {
+            get
+            {
+                return _dictionary[key];
+            }
+
+            set
+            {
+                _dictionary[key] = value;
+            }
+        }
+    }
+}

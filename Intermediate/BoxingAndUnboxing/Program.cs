2cdf3f4372e3e2b6ba3dc202634d0e8c189d3885
PradipJathar <jatharpradipofficial@gmail.com>
2023-09-28 16:34:02 +0530

    Boxing and Unboxing.

---
+++ Intermediate/BoxingAndUnboxing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingAndUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+            object obj = number;            // Boxing - Conversion of a value type to reference type.
+
+            object obj1 = 20;
+            int number1 = (int) obj1;       // Unboxing - Conversion of a reference type to value type.
+
+        }
+    }
+}

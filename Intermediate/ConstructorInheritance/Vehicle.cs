3dc0700acf388ac0b34db56a6144f5aec0680c21
PradipJathar <jatharpradipofficial@gmail.com>
2023-09-28 15:47:32 +0530

    Constructor Inheritance.

---
+++ Intermediate/ConstructorInheritance/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConstructorInheritance
+{
+    public class Vehicle
+    {
+        private readonly string _registrationNumber;
+        public Vehicle(string registrationNumber)                   // Base class constructor are always executed first.
+        {                                                           // Base class constructor are not inherited.
+            _registrationNumber = registrationNumber;
+
+            Console.WriteLine($"Vehicle is being initialized. {registrationNumber}");
+        }
+    }
+}

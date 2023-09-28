81c5abfdf84a21bf0e273879265dd9deda384319
PradipJathar <jatharpradipofficial@gmail.com>
2023-09-28 19:27:45 +0530

    Exercise - Design a Stack.

---
+++ Intermediate/ExerciseDesignStack/MyStack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ExerciseDesignStack
+{
+    public class MyStack
+    {
+        private readonly List<object> list = new List<object>();
+
+        public void Push(object item)
+        {
+            if (item == null)
+            {
+                throw new InvalidOperationException("You can not add a null object to the stack.");
+            }
+
+            list.Add(item);
+        }
+
+        public object Pop()
+        {
+            if (list.Count == 0)
+            {
+                throw new InvalidOperationException("There are no elements in the stack.");
+            }
+
+            object lastItem = list[list.Count - 1];
+            list.Remove(lastItem);
+
+            return lastItem;
+        }
+
+
+        public void Clear()
+        {           
+            list.Clear();
+        }
+    }
+}
---
+++ Intermediate/ExerciseDesignStack/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseDesignStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack stack = new MyStack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Clear();
+            stack.Push(3);
+            stack.Push("Pradip");
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}

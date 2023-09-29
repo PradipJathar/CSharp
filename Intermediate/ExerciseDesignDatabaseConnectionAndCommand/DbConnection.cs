d9eb64e0a0e079e22dedb14e9ba01dfbc0749743
PradipJathar <jatharpradipofficial@gmail.com>
2023-09-29 18:06:54 +0530

    Exercise - Design a Database Connection.

---
+++ Intermediate/ExerciseDesignDatabaseConnectionAndCommand/DbConnection.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ExerciseDesignDatabaseConnectionAndCommand
+{
+    public abstract class DbConnection
+    {
+        private string ConnectionString { get; set; }
+        private TimeSpan Timeout { get; set; }
+
+
+        public DbConnection(string connectionString)
+        {
+            if (String.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new InvalidOperationException("Please provide connection string.");
+            }
+
+            this.ConnectionString = connectionString;
+            this.Timeout = TimeSpan.FromSeconds(30);            // Default timeout of 30 seconds
+        }
+
+
+        public abstract void Open();
+
+        public abstract void Close();
+
+    }
+
+
+    public class SqlConnection : DbConnection
+    {
+        public SqlConnection(string connectionString) : base(connectionString)
+        {
+
+        }
+
+        public override void Open()
+        {
+            Console.WriteLine("Sql Connection Opened.");
+        }
+
+        public override void Close()
+        {
+            Console.WriteLine("Sql Connection Closed.");
+        }
+    }
+
+
+    public class OracleConnection : DbConnection
+    {
+        public OracleConnection(string connectionString) : base(connectionString)
+        {
+
+        }
+
+        public override void Open()
+        {
+            Console.WriteLine("Oracle Connection Opened.");
+        }
+
+        public override void Close()
+        {
+            Console.WriteLine("Oracle Connection Closed.");
+        }
+    }
+}
---
+++ Intermediate/ExerciseDesignDatabaseConnectionAndCommand/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseDesignDatabaseConnectionAndCommand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection sqlConnection = new SqlConnection("abcd123");
+            sqlConnection.Open();
+            sqlConnection.Close();
+
+            OracleConnection oracleConnection = new OracleConnection("ab12cd34");
+            oracleConnection.Open();
+            oracleConnection.Close();
+
+        }
+    }
+}

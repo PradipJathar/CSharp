b402a26a5e5d9af1ab1427f0a35b45ca814f7480
PradipJathar <jatharpradipofficial@gmail.com>
2023-09-29 16:44:38 +0530

    Abstract Classes.

---
+++ Intermediate/AbstractClasses/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Shape shape = new Shape();       // Abstract class can not be instantiated.
+
+            Circle circle = new Circle();
+            circle.Draw();                      // Draw() - Abstract Method.
+            circle.Copy();                      // Copy() - Non-Abstract Method.
+
+
+            Rectangle rectangle = new Rectangle();
+            rectangle.Draw();
+            rectangle.Copy();
+
+        }
+    }
+}
---
+++ Intermediate/AbstractClasses/Shape.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace AbstractClasses
+{
+
+    // Abstract Class:
+    // Abstract Modifier - Indicates that a class or a member is missing implementation.
+    // Abstract members does not include implementation, they will be implement in derived classes.
+    // If a member is declared as abstract, the containing class needs to be declared as abstract too.
+    // Derived classes must implement all abstract members in base abstract class.
+    // Abstract class can not be instantiated.
+    // Abstract class can have non abstract members.
+
+
+    public abstract class Shape                                 // If a member is declared as abstract, the containing class needs to be declared as abstract too.
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public abstract void Draw();                            // Abstract members does not include implementation, they will be implement in derived classes.
+
+        public void Copy()                                      // Abstract class can have non-abstract members.
+        {
+            Console.WriteLine("Copy shape into clipboard.");
+        }
+    }
+
+
+
+    public class Circle : Shape
+    {
+        public override void Draw()                             // Derived classes must implement all abstract members in base abstract class.
+        {
+            Console.WriteLine("Draw a Circle.");
+        }
+    }
+
+
+
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a Rectangle.");
+        }
+    }
+}

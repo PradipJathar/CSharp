6d1428dcbffe8517261b33894d089b800e63aecf
PradipJathar <jatharpradipofficial@gmail.com>
2023-10-03 20:23:01 +0530

    Interfaces and Testability.

---
+++ Intermediate/Testability.UnitTests/OrderProcessorTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testability.UnitTests
+{
+    [TestClass]
+    public class OrderProcessorTests
+    {
+
+        //MethodName_Condition_Expectation
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Process_OrderIsAlreadyShipped_ThrowsAnException()
+        {
+            OrderProcessor orderProcessor = new OrderProcessor(new FakeShippingCalculator());
+
+            Order order = new Order
+            {
+                Shipment = new Shipment()
+            };
+
+            orderProcessor.Process(order);
+        }
+
+
+        [TestMethod]
+        public void Process_OrderIsNotShipped_ShouldSetTheShipmentPropertyOfTheOrder()
+        {
+            OrderProcessor orderProcessor = new OrderProcessor(new FakeShippingCalculator());
+            Order order = new Order();
+
+            orderProcessor.Process(order);
+
+            Assert.IsTrue(order.IsShipped);
+            Assert.AreEqual(1, order.Shipment.Cost);
+            Assert.AreEqual(DateTime.Now.AddDays(1), order.Shipment.ShippingDate);
+
+        }
+    }
+
+    public class FakeShippingCalculator : IShippingCalculator
+    {
+        public float CalculateShipping(Order order)
+        {
+            return 1;
+        }
+    }
+}
---
+++ Intermediate/Testability.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Testability.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Testability.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ccee52e5-526c-43ab-8609-505784e9a16b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

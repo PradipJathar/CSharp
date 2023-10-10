be3190ee243697c32ffeb26deca9aca3b8d40369
PradipJathar <jatharpradipofficial@gmail.com>
2023-10-10 15:14:57 +0530

    Lambda Expressions

---
+++ Advance/LambdaExpressions/BookRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace LambdaExpressions
+{
+    public class BookRepository
+    {
+        public List<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book(){Title = "Bhagwat Gita",  Price = 299},
+                new Book(){Title = "Ramayan",  Price = 499},
+                new Book(){Title = "Mahabharat",  Price = 799},
+                new Book(){Title = "Rich Dad Poor Dad",  Price = 125}
+            };
+        }
+    }
+}
---
+++ Advance/LambdaExpressions/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpressions
+{
+    // Lambda Expressions:
+    // An anonymous method with no access modifier, no name, and no return statement.
+    // args => expression
+
+    // Lambda Expression Types:
+    // 1. () => 2 * 3                                   --> Without arguments.
+    // 2. x => x.Price * x.Quantity                     --> With one argument.
+    // 3. (x, y, z) => x.Price + y.Price + z.Price      --> With multiple arguments.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Func<int, int> areaOfSquare = side => side * side;             // Lambda Expressions with delegate.
+            Console.WriteLine($"Area of Square : {areaOfSquare(25)}");
+
+
+            int rate = 10;
+            int years = 3;
+
+            Func<int, int> interest = principalAmount => principalAmount * rate * years;
+            Console.WriteLine($"Interest Amount : {interest(10000)}");
+
+
+            Console.WriteLine();
+
+
+            List<Book> books = new BookRepository().GetBooks();
+            List<Book> cheapBooks = books.FindAll(b => b.Price < 300);          // Lambda Expressions
+
+            foreach (var book in cheapBooks)
+            {
+                Console.WriteLine(book.Title);
+            }
+
+        }
+    }
+}

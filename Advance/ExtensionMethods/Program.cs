3c34ccb88841e6b9bf1ff63bf8ee3de7b6136a63
PradipJathar <jatharpradipofficial@gmail.com>
2023-10-10 17:52:45 +0530

    Extension Methods

---
+++ Advance/ExtensionMethods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    // Extension Methods
+    // Allow us to add methods to an existing class without
+    // - Changing its source code or
+    // - Creating a new class that inherits form it.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "This is supposed to be very very very long post.";
+
+            string shortPost = post.Shorten(5);                                // Shorten() - Extension Methods
+
+            Console.WriteLine(shortPost);
+        }
+    }
+}
---
+++ Advance/ExtensionMethods/StringExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace System
+{
+    public static class StringExtensions
+    {
+        public static string Shorten(this String str, int numberOfWords)
+        {
+            if (numberOfWords < 0)
+            {
+                throw new ArgumentOutOfRangeException("numberOfWords", "Number of words should be greater than or equal to 0.");
+            }
+
+            if (numberOfWords == 0)
+            {
+                return "";
+            }
+
+            string[] words = str.Split(' ');
+
+            if (words.Length <= numberOfWords)
+            {
+                return str;
+            }
+
+            return String.Join(" ", words.Take(numberOfWords)) + "...";
+        }
+    }
+}

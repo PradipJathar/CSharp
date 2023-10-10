98bcc59c508bacdba73aa894638b6d4f2d263663
PradipJathar <jatharpradipofficial@gmail.com>
2023-10-10 12:53:12 +0530

    Delegates

---
+++ Advance/Delegates/PhotoProcessor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Delegates
+{
+    // Delegates
+    // A delegate is a reference type variable that holds the reference to a methods.
+    // An object that used to call a method or a group of methods.
+    // Used for designing extensible and flexible applications.
+    // It use just like Interfaces, but used when caller doesn't need to access other properties or methods on the object implementing the method.
+
+    public class PhotoProcessor
+    {
+
+        // 1] User defined delegate.
+        public delegate void PhotoFilterHandler(Photo photo);       // Delegate
+
+        public void Process(string path, PhotoFilterHandler filterHandler)
+        {
+            var photo = Photo.Load(path);
+
+            filterHandler(photo);
+
+            photo.Save();
+        }
+
+
+        // 2] .Net predefined delegates.
+        //    A. System.Action<T>  --> void
+        //    B. System.Func<T>    --> return value
+
+        public void Process(string path, Action<Photo> filterHandler)
+        {
+            var photo = Photo.Load(path);
+
+            filterHandler(photo);
+
+            photo.Save();
+        }
+
+    }
+}
---
+++ Advance/Delegates/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PhotoProcessor processor = new PhotoProcessor();
+            PhotoFilters filters = new PhotoFilters();
+
+            // 1] User defined delegate.
+
+            PhotoProcessor.PhotoFilterHandler filterHandler = filters.ApplyBrightness;      // Adding methods in delegate.
+            filterHandler += filters.Resize;
+
+            processor.Process("photo.jpg", filterHandler);
+
+
+            Console.WriteLine();
+
+
+            // 2] .Net predefined delegates.
+
+            Action<Photo> actionFilterHandler = filters.ApplyContrast;                      // Adding methods in predefined delegate.
+            actionFilterHandler += filters.ApplyBrightness;
+            actionFilterHandler += filters.Resize;
+
+            processor.Process("photo.jpg", actionFilterHandler);
+
+        }
+    }
+}
